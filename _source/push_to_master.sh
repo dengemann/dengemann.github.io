98bf6fd827396065e621bb1be9b34f7f068161fa
dengemann <denis.engemann@gmail.com>
2016-10-15 17:35:35 +0100

    updates

--- _source/push_to_master.sh
+++ _source/push_to_master.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env/bash
 rsync -avzh output ../
 
-cd (../ &&
-    git ci -am 'updates' &&
-    git commit -m 'rebuild pages' --allow-empty &&
-    git push origin master)
+git ci -am 'updates' &&
+git commit -m 'rebuild pages' --allow-empty &&
+git push origin master
